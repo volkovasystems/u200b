b789d6c65b14309e8d40b3c170256bdbd884f183
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-11 23:26:39 +0800

    Improve build procedure,

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+"use strict";
+
 const webpack = require( "webpack" );
+const ResolverPlugin = webpack.ResolverPlugin;
+const DirectoryDescriptionFilePlugin = ResolverPlugin.DirectoryDescriptionFilePlugin;
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 module.exports = {
 	"entry": "./u200b.support.js",
@@ -19,9 +24,18 @@ module.exports = {
 		"filename": "u200b.deploy.js"
 	},
 	"plugins": [
-		new webpack.ResolverPlugin( new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
-		new webpack.ResolverPlugin( new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
-		new webpack.optimize.UglifyJsPlugin( { "compress": { "warnings": false }, "comments": false, "sourceMap": true } )
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
+		new ResolverPlugin( new DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
+		new UglifyJsPlugin( {
+			"compress": {
+				"keep_fargs": true,
+				"keep_fnames": true,
+				"warnings": false
+			},
+			"comments": false,
+			"sourceMap": true,
+			"mangle": false
+		} )
 	],
 	"devtool": "#inline-source-map"
 };
