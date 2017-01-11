933e282ce158148aac8743b397dd081889ef67e8
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-11 22:49:49 +0800

    Improved codes.

--- u200b.js
+++ u200b.js
@@ -5,7 +5,7 @@
 		The MIT License (MIT)
 		@mit-license
 
-		Copyright (@c) 2016 Richeve Siodina Bebedor
+		Copyright (@c) 2017 Richeve Siodina Bebedor
 		@email: richeve.bebedor@gmail.com
 
 		Permission is hereby granted, free of charge, to any person obtaining a copy
