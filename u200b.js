8a12f718463089e4dcf33b077f3e259f99280094
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-15 19:02:23 +0800

    Updated server/client test.

--- u200b.js
+++ u200b.js
@@ -35,7 +35,8 @@
 			"module": "u200b",
 			"author": "Richeve S. Bebedor",
 			"contributors": [
-				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
 			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/u200b.git",
