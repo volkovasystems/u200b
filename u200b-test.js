c917eddba213e02f5125ed516f66654705df78d3
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 16:52:09 +0800

    Updated test.

--- u200b-test.js
+++ u200b-test.js
@@ -2,11 +2,11 @@
 const assert = require( "assert" );
 const U200b = require( "./u200b.js" );
 
-assert.ok( U200b( "hello", "world" ) );
+assert.ok( U200b( "hello", "world" ), "should not throw error" );
 
-assert.equal( U200b( "hello", "world" ).join( "." ), "hello​.world", "should return 'hello​.world'" );
+assert.equal( U200b( "hello", "world" ).join( "." ), "hello​.world", "should be equal to 'hello​.world'" );
 
 let test = U200b( "hello", "world" ).join( "." );
-assert.equal( U200b( test ).replace( ".", "_" ), "hello_world", "should return 'hello​_world'" );
+assert.equal( U200b( test ).replace( ".", "_" ), "hello_world", "should be equal to 'hello​_world'" );
 
 console.log( "ok" );
